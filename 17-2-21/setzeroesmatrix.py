b05231555b5f1982e2b0f326720ca0a3ce3e4fa3
bamblebam <burhanizrangwala@gmail.com>
2021-02-17 22:55:10 +0530

    17-2-21/setzeroesmatrix:

--- 17-2-21/setzeroesmatrix.py
+++ 17-2-21/setzeroesmatrix.py
@@ -5,7 +5,21 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
+        is_col = False
         for i in range(m):
-            for j in range(n):
+            if matrix[i][0] == 0:
+                is_col = True
+            for j in range(1, n):
                 if matrix[i][j] == 0:
-                    matrix[i] =
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(n):
+                matrix[0][j] = 0
+        if is_col:
+            for i in range(m):
+                matrix[i][0] = 0
