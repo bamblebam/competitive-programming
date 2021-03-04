83ddb93cdd9835283b614bc1dd5b99315451bc27
bamblebam <burhanizrangwala@gmail.com>
2021-03-05 03:39:37 +0530

    3_March_21/4-3-21/connecttreenodes: create

---
+++ 3_March_21/4-3-21/connecttreenodes.py
@@ -0,0 +1,27 @@
+# Definition for a Node.
+# class Node:
+#     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.next = next
+
+
+class Solution:
+    def connect(self, root):
+        if root is None:
+            return None
+        queue = list()
+        queue.append(root)
+        queue.append(None)
+        while queue:
+            node = queue.pop(0)
+            if node:
+                node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            elif queue:
+                queue.append(None)
+        return root
