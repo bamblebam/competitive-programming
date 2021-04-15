5fa513f471738e9eb0fd4cd6602902d476dc5fa7
bamblebam <burhanizrangwala@gmail.com>
2021-04-15 22:26:41 +0530

    4_April_21/14-4-21/partitionlinkedlist: create

---
+++ 4_April_21/14-4-21/partitionlinkedlist.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def partition(self, head, x):
+        before = beforehead = ListNode(0)
+        after = afterhead = ListNode(0)
+        while head is not None:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        after.next = None
+        before.next = afterhead.next
+        return beforehead.next
