1ef64614407a6af716218424a086890a33ad7145
bamblebam <burhanizrangwala@gmail.com>
2021-04-19 09:17:03 +0530

    4_April_21/18-4-21/removenthfromend: create

---
+++ 4_April_21/18-4-21/removenthfromend.py
@@ -0,0 +1,19 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        temp = ListNode(0)
+        temp.next = head
+        first, second = temp, temp
+        for i in range(n+1):
+            first = first.next
+        while first is not None:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return temp.next
