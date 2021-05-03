a2557e0174a5bbe32225dd36de308b499ae41892
bamblebam <burhanizrangwala@gmail.com>
2021-05-03 17:01:33 +0530

    5_May_21/2-5-21/course_schedule: create

---
+++ 5_May_21/2-5-21/course_schedule.py
@@ -0,0 +1,13 @@
+import heapq
+
+
+class Solution:
+    def scheduleCourse(self, courses) -> int:
+        heap, time = [], 0
+        for t, end in sorted(courses, key=lambda x: x[1]):
+            time += t
+            heapq.heappush(heap, -t)
+            if time > end:
+                nt = heapq.heappop(heap)
+                time += nt
+        return len(heap)
