1077fd573f6b10219943d9617a0c92072782c277
bamblebam <burhanizrangwala@gmail.com>
2021-05-10 14:39:46 +0530

    5_May_21/10-5-21/countprimes:

--- 5_May_21/10-5-21/countprimes.py
+++ 5_May_21/10-5-21/countprimes.py
@@ -8,6 +8,6 @@ class Solution:
         numbers = set()
         for i in range(2, int(math.sqrt(n))+1):
             if i not in numbers:
-                for j in range(i*i, n, i)
-                numbers.add(j)
+                for j in range(i*i, n, i):
+                    numbers.add(j)
         return n-2-len(numbers)
