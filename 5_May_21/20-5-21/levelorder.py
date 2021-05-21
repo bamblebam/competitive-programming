ddcadf459954a568918c747817d312ea7e1e8f97
bamblebam <burhanizrangwala@gmail.com>
2021-05-21 12:59:36 +0530

    5_May_21/20-5-21/levelorder: create

---
+++ 5_May_21/20-5-21/levelorder.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        if root is None:
+            return list()
+        queue, answer = deque([root]), list()
+        while queue:
+            temp = list()
+            for i in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            answer.append(temp)
+        return answer
