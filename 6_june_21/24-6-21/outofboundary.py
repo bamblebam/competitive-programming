6d22ba408933c88e2715f767873fdd6d0cf41f51
bamblebam <burhanizrangwala@gmail.com>
2021-07-05 22:22:55 +0530

    6_june_21/24-6-21/outofboundary:

--- 6_june_21/24-6-21/outofboundary.py
+++ 6_june_21/24-6-21/outofboundary.py
@@ -15,3 +15,17 @@ class Solution:
                                                        col+1)+helper(moves-1, row-1, col)+helper(moves-1, row+1, col)
             return total % mod
         return helper(maxMove, startRow, startColumn)
+
+# alternate Solution
+# class Solution:
+#     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+#         mod=10**9+7
+#         @lru_cache(None)
+#         def helper(moves,row,col):
+#             if moves<0:
+#                 return 0
+#             if row<0 or row>=m or col<0 or col>=n:
+#                 return 1
+#             total=helper(moves-1,row+1,col)+helper(moves-1,row-1,col)+helper(moves-1,row,col+1)+helper(moves-1,row,col-1)
+#             return total%mod
+#         return helper(maxMove,startRow,startColumn)%mod
