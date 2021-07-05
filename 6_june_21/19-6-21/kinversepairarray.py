e9d8a7ae6ad347858a251333761fd39ae019c52a
bamblebam <burhanizrangwala@gmail.com>
2021-07-05 07:17:12 +0530

    6_june_21/19-6-21/kinversepairarray:

--- 6_june_21/19-6-21/kinversepairarray.py
+++ 6_june_21/19-6-21/kinversepairarray.py
@@ -11,3 +11,15 @@ class Solution:
                            if j-i >= 0 else 0)) % mod
                     dp[i][j] = (dp[i][j-1]+val) % mod
         return (dp[n][k]+mod-(dp[n][k-1] if k > 0 else 0)) % mod
+
+# alternate solution
+# class Solution:
+#     def kInversePairs(self, n: int, k: int) -> int:
+#         mod=10**9+7
+#         dp=[[1]*(k+1) for _ in range(n+1)]
+#         sp=[[1]*(k+1) for _ in range(n+1)]
+#         for i in range(1,n+1):
+#             for j in range(1,k+1):
+#                 dp[i][j]=sp[i-1][j] if j<i else (sp[i-1][j]-sp[i-1][j-i])%mod
+#                 sp[i][j]=(sp[i][j-1]+dp[i][j])%mod
+#         return dp[-1][-1]
