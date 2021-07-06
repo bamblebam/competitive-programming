06d126fe21e46f33d6e83a844b5e1dca3600ec4e
bamblebam <burhanizrangwala@gmail.com>
2021-07-06 18:01:08 +0530

    5_May_21/5-5-21/jumpgame2:

--- 5_May_21/5-5-21/jumpgame2.py
+++ 5_May_21/5-5-21/jumpgame2.py
@@ -10,3 +10,16 @@ class Solution:
             index = d[index]
             count += 1
         return count
+
+# alternate Solution
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         n=len(nums)
+#         nums[-1]=0
+#         for i in range(n-2,-1,-1):
+#             val=nums[i]
+#             if val==0:
+#                 nums[i]=float('inf')
+#             else:
+#                 nums[i]=min(nums[i:i+val+1])+1 if i==n-2 else min(nums[i+1:i+val+1])+1
+#         return nums[0]
