bf05bfa7a7eb198012d6dee8925c515354a0eb07
bamblebam <burhanizrangwala@gmail.com>
2021-07-09 01:11:07 +0530

    4_April_21/9-4-21/isaliensorted:

--- 4_April_21/9-4-21/isaliensorted.py
+++ 4_April_21/9-4-21/isaliensorted.py
@@ -12,3 +12,30 @@ class Solution:
                         return False
                     break
         return True
+
+# alternate Solution
+# class Solution:
+#     def isAlienSorted(self, words: List[str], order: str) -> bool:
+#         idx_map=dict()
+#         for i,letter in enumerate(order):
+#             idx_map[letter]=i
+#         for i in range(1,len(words)):
+#             w1=words[i-1]
+#             w2=words[i]
+#             n1=len(w1)
+#             n2=len(w2)
+#             i,j=0,0
+#             flag=False
+#             while i<n1 and j<n2:
+#                 char1,char2=w1[i],w2[j]
+#                 print(char1,char2)
+#                 if idx_map[char1]>idx_map[char2]:
+#                     return False
+#                 elif idx_map[char1]<idx_map[char2]:
+#                     flag=True
+#                     break
+#                 i+=1
+#                 j+=1
+#             if not flag and n1>n2:
+#                 return False
+#         return True
