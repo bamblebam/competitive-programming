79ea269dd7018a41b179b545a6fe87b032a261c3
bamblebam <burhanizrangwala@gmail.com>
2021-07-19 01:27:20 +0530

    7_july_21/19-7-21/decodeways: create

---
+++ 7_july_21/19-7-21/decodeways.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [1]+[0]*n
+        for i in range(n):
+            # for 1 digit
+            curr = s[i]
+            if '1' <= curr <= '9':
+                dp[i+1] += dp[i]
+            # for 2 digit
+            if i-1 >= 0:
+                prev = s[i-1]
+                if prev == '1':
+                    dp[i+1] += dp[i-1]
+                elif prev == '2':
+                    if '0' <= curr <= '6':
+                        dp[i+1] += dp[i-1]
+        return dp[n]
