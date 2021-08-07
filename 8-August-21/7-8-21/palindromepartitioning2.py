307bbc04fd80f6c194871649c3927383ebc076ba
bamblebam <burhanizrangwala@gmail.com>
2021-08-07 23:38:31 +0530

    8-August-21/7-8-21/palindromepartitioning2: create

---
+++ 8-August-21/7-8-21/palindromepartitioning2.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+from collections import defaultdict
+
+class Solution:
+    def minCut(self, s: str) -> int:
+        d=defaultdict(set)
+        n=len(s)
+        
+        def helper(i,j):
+            while i>=0 and j<n and s[i]==s[j]:
+                d[i].add(j)
+                i-=1
+                j+=1
+        
+        for k in range(n):
+            helper(k,k)
+            helper(k,k+1)
+        
+        @lru_cache(None)
+        def dp(i):
+            if i==-1:
+                return 0
+            return min([dp(k-1)+1 for k in range(0,i+1) if i in d[k]])
+        
+        return dp(n-1)-1
