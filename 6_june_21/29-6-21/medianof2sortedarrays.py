7b0f2ed0f770fea7723cbb33b7ec1142648f4889
bamblebam <burhanizrangwala@gmail.com>
2021-08-14 17:30:52 +0530

    6_june_21/29-6-21/medianof2sortedarrays:

--- 6_june_21/29-6-21/medianof2sortedarrays.py
+++ 6_june_21/29-6-21/medianof2sortedarrays.py
@@ -1,22 +1,45 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-        if len(nums2)<len(nums1):
-            nums1,nums2=nums2,nums1
-        m,n=len(nums1),len(nums2)
-        half=(m+n)//2
-        l,r=0,m-1
+        if len(nums2) < len(nums1):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        half = (m+n)//2
+        l, r = 0, m-1
         while True:
-            i=(l+r)//2
-            j=half-i-2
-            left1=nums1[i] if i>=0 else -float('inf')
-            left2=nums2[j] if j>=0 else -float('inf')
-            right1=nums1[i+1] if (i+1)<m else float('inf')
-            right2=nums2[j+1] if (j+1)<n else float('inf')
-            if left1<=right2 and left2<=right1:
-                if (m+n)%2:
-                    return min(right1,right2)
-                return (max(left1,left2)+min(right1,right2))/2
-            elif left1>right2:
-                r=i-1
+            i = (l+r)//2
+            j = half-i-2
+            left1 = nums1[i] if i >= 0 else -float('inf')
+            left2 = nums2[j] if j >= 0 else -float('inf')
+            right1 = nums1[i+1] if (i+1) < m else float('inf')
+            right2 = nums2[j+1] if (j+1) < n else float('inf')
+            if left1 <= right2 and left2 <= right1:
+                if (m+n) % 2:
+                    return min(right1, right2)
+                return (max(left1, left2)+min(right1, right2))/2
+            elif left1 > right2:
+                r = i-1
             else:
-                l=i+1
+                l = i+1
+
+# class Solution:
+#     def findMedianSortedArrays(self, nums1, nums2) -> float:
+#         if len(nums2)<len(nums1):
+#             nums1,nums2=nums2,nums1
+#         m,n=len(nums1),len(nums2)
+#         half=(m+n)//2
+#         l,r=0,m-1
+#         while True:
+#             i=(l+r)//2
+#             j=half-i-2
+#             left1=nums1[i] if i>=0 else -float('inf')
+#             left2=nums2[j] if j>=0 else -float('inf')
+#             right1=nums1[i+1] if (i+1)<m else float('inf')
+#             right2=nums2[j+1] if (j+1)<n else float('inf')
+#             if left1<=right2 and left2<=right1:
+#                 if (m+n)%2:
+#                     return min(right1,right2)
+#                 return (max(left1,left2)+min(right1,right2))/2
+#             elif left1>right2:
+#                 r=i-1
+#             else:
+#                 l=i+1
