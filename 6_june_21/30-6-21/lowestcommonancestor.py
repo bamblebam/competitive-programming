0e0c6a7ca7de7e3b4a5a1ea1395a6acf5c6e3681
bamblebam <burhanizrangwala@gmail.com>
2021-08-14 17:59:34 +0530

    6_june_21/30-6-21/lowestcommonancestor:

--- 6_june_21/30-6-21/lowestcommonancestor.py
+++ 6_june_21/30-6-21/lowestcommonancestor.py
@@ -21,3 +21,27 @@ class Solution:
             return curr or left or right
         helper(root)
         return self.ans
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# class Solution:
+#     def __init__(self):
+#         self.ans=None
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         p,q=p.val,q.val
+#         def helper(node):
+#             if node is None:
+#                 return False
+#             left=helper(node.left)
+#             right=helper(node.right)
+#             curr=p==node.val or q==node.val
+#             if curr+left+right>=2:
+#                 self.ans=node
+#             return curr or left or right
+#         helper(root)
+#         return self.ans
