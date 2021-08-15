31583ec3e6a9fb97a41711738ffbbe18d719cec1
bamblebam <burhanizrangwala@gmail.com>
2021-08-15 15:55:16 +0530

    6_june_21/4-6-21/minimumwindowsubstring:

--- 6_june_21/4-6-21/minimumwindowsubstring.py
+++ 6_june_21/4-6-21/minimumwindowsubstring.py
@@ -27,3 +27,27 @@ class Solution:
                 l += 1
             r += 1
         return "" if ans[0] == float('inf') else s[ans[1]:ans[2]+1]
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if len(t)>len(s) or s=="" or t=="":
+#             return ""
+#         count_t=Counter(t)
+#         s_map=[(i,x) for i,x in enumerate(s) if x in count_t]
+#         completed=0
+#         ans=(None,None,float('inf'))
+#         l,r=0,0
+#         count_s=defaultdict(lambda: 0)
+#         for end,char1 in s_map:
+#             count_s[char1]+=1
+#             if count_s[char1]==count_t[char1]:
+#                 completed+=1
+#             while completed==len(count_t):
+#                 start,char2=s_map[l]
+#                 if ans[2]>end-start+1:
+#                     ans=(start,end,end-start+1)
+#                 count_s[char2]-=1
+#                 if count_s[char2]<count_t[char2]:
+#                     completed-=1
+#                 l+=1
+#         return s[ans[0]:ans[1]+1] if ans[2]!=float('inf') else ""
