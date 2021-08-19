bb746a35577cf4c26a0fcefbda6ca6a78c55c05c
bamblebam <burhanizrangwala@gmail.com>
2021-08-19 13:59:55 +0530

    7_july_21/19-7-21/decodeways:

--- 7_july_21/19-7-21/decodeways.py
+++ 7_july_21/19-7-21/decodeways.py
@@ -16,3 +16,23 @@ class Solution:
                     if '0' <= curr <= '6':
                         dp[i+1] += dp[i-1]
         return dp[n]
+
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
+#         n=len(s)
+#         dp=[1]+[0]*n
+#         for i in range(n):
+#             curr=s[i]
+#             #for 1 digit
+#             if '1'<=curr<='9':
+#                 dp[i+1]+=dp[i]
+#             #for 2 digit
+#             if i-1>=0:
+#                 prev=s[i-1]
+#                 if prev=='1':
+#                     dp[i+1]+=dp[i-1]
+#                 elif prev=='2':
+#                     if '0'<=curr<='6':
+#                         dp[i+1]+=dp[i-1]
+#         return dp[n]
+        
