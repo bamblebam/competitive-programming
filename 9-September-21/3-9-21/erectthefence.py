9c54c17e848bfefb2c4940c5e2a8bbec491f35f0
bamblebam <burhanizrangwala@gmail.com>
2021-09-05 13:15:20 +0530

    9-September-21/3-9-21/erectthefence: create

---
+++ 9-September-21/3-9-21/erectthefence.py
@@ -0,0 +1,37 @@
+class Solution:
+    def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
+        trees.sort()
+        # monotone chain covex hull
+        lower = list()
+        for x, y in trees:
+            if len(lower) < 2:
+                lower.append((x, y))
+                continue
+            while len(lower) >= 2:
+                ax, ay = lower[-2]
+                bx, by = lower[-1]
+                cx, cy = x, y
+                area = ax*by+bx*cy+cx*ay-bx*cy-cx*by-ax*cy
+                if area < 0:
+                    lower.pop()
+                else:
+                    break
+            lower.append((cx, cy))
+
+        upper = list()
+        for x, y in trees:
+            if len(upper) < 2:
+                upper.append((x, y))
+                continue
+            while len(upper) >= 2:
+                ax, ay = upper[-2]
+                bx, by = upper[-1]
+                cx, cy = x, y
+                area = ax*by+bx*cy+cx*ay-bx*cy-cx*by-ax*cy
+                if area > 0:
+                    upper.pop()
+                else:
+                    break
+            upper.append((cx, cy))
+
+        return set(lower) | set(upper)
