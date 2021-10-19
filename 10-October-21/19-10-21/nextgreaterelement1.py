7433746d5dd6ccde65dd1e6d887689f422778532
bamblebam <burhanizrangwala@gmail.com>
2021-10-19 18:04:56 +0530

    10-October-21/19-10-21/nextgreaterelement1:

--- 10-October-21/19-10-21/nextgreaterelement1.py
+++ 10-October-21/19-10-21/nextgreaterelement1.py
@@ -12,3 +12,24 @@ class Solution:
             if not flag:
                 ans.append(-1)
         return ans
+    
+        # #alternate solution O(n)
+        # #use monotone stack
+        # #iterate through reverse nums2 and if the curr element is greater than the latest one in the stack pop it. The remaining stack[-1] will be the answer. if stack is empty it is -1.
+        # #if number is less than stcak[-1] just add it to stack and its answer is stack[-1]
+        # idx_map=dict()
+        # stack=list()
+        # m,n=len(nums1),len(nums2)
+        
+        # for i in range(n-1,-1,-1):
+        #     curr=nums2[i]
+        #     while stack and stack[-1]<curr:
+        #         stack.pop()
+        #     if not stack:
+        #         idx_map[curr]=-1
+        #         stack.append(curr)
+        #         continue
+        #     idx_map[curr]=stack[-1]
+        #     stack.append(curr)
+        
+        # return list(idx_map[x] for x in nums1)
