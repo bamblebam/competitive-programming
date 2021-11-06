28ae1e52df90582e4a402f47d812ec5ff8a9d50a
bamblebam <burhanizrangwala@gmail.com>
2021-11-06 23:16:09 +0530

    6_june_21/29-6-21/maxconsecutiveones3:

--- 6_june_21/29-6-21/maxconsecutiveones3.py
+++ 6_june_21/29-6-21/maxconsecutiveones3.py
@@ -15,4 +15,26 @@ class Solution:
             right+=1
         return ans
             
+# #alternate solution
+# class Solution:
+#     def longestOnes(self, nums: List[int], k: int) -> int:
+#         n=len(nums)
+#         ans=0
+#         l,r=0,0
+#         while r<n:
+#             if nums[r]==1:
+#                 r+=1
+#                 ans=max(r-l,ans)
+#             elif nums[r]==0 and k>0:
+#                 r+=1
+#                 k-=1
+#                 ans=max(r-l,ans)
+#             elif nums[r]==0:
+#                 while k<=0:
+#                     if nums[l]==0:
+#                         k+=1
+#                     l+=1
+#                 ans=max(r-l,ans)
+#         ans=max(r-l,ans)
+#         return ans
                 
