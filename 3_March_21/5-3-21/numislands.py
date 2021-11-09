f54cf5d8a2f8b9489d11bfd567ab177668538ccf
bamblebam <burhanizrangwala@gmail.com>
2021-11-09 15:54:59 +0530

    3_March_21/5-3-21/numislands:

--- 3_March_21/5-3-21/numislands.py
+++ 3_March_21/5-3-21/numislands.py
@@ -18,3 +18,28 @@ class Solution:
                     answer += 1
                     self.helper(grid, i, j)
         return answer
+    
+    #alternative solution
+    # class Solution:
+    # def numIslands(self, grid: List[List[str]]) -> int:
+    #     #ans is for size of largest island
+    #     #do dfs on each island and add the count to parent func. via recursion you will get size of island
+    #     #num_island is for number of islands
+    #     m,n=len(grid),len(grid[0])
+    #     ans=0
+    #     num_islands=0
+    #     dirs=[(0,1),(0,-1),(1,0),(-1,0)]
+    #     def dfs(x,y):
+    #         count=1
+    #         grid[x][y]=0
+    #         for dx,dy in dirs:
+    #             nx,ny=x+dx,y+dy
+    #             if 0<=nx<m and 0<=ny<n and grid[nx][ny]=='1':
+    #                 count+=dfs(nx,ny)
+    #         return count
+    #     for i in range(m):
+    #         for j in range(n):
+    #             if grid[i][j]=="1":
+    #                 ans=max(ans,dfs(i,j))
+    #                 num_islands+=1
+    #     return num_islands
