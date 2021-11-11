3a45d5712b3ee53c5cd66ef2c9a9630676f5b0b8
bamblebam <burhanizrangwala@gmail.com>
2021-11-11 12:23:59 +0530

    7_july_21/19-7-21/besttimetobuyandsellstock2:

--- 7_july_21/19-7-21/besttimetobuyandsellstock2.py
+++ 7_july_21/19-7-21/besttimetobuyandsellstock2.py
@@ -6,3 +6,24 @@ class Solution:
             if prices[i+1] > prices[i]:
                 profit += prices[i+1]-prices[i]
         return profit
+
+#alternate solution
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         #use peak and value to discover the smaleest element first then greatest and so on
+#         ans=0
+#         n=len(prices)
+#         peak=0
+#         valley=prices[0]
+#         i=1
+#         while i<n:
+#             while i<n and prices[i-1]>=prices[i]:
+#                 valley=prices[i]
+#                 i+=1
+#             while i<n and prices[i-1]<prices[i]:
+#                 peak=prices[i]
+#                 i+=1
+#             if peak>valley:
+#                 ans+=peak-valley
+#             peak=valley=0
+#         return ans
