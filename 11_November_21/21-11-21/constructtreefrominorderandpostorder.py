b48679c314b1d33f02415df0c64e76d579f0b873
bamblebam <burhanizrangwala@gmail.com>
2021-11-21 22:23:56 +0530

    11_November_21/21-11-21/constructtreefrominorderandpostorder: create

---
+++ 11_November_21/21-11-21/constructtreefrominorderandpostorder.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        #just like with preorder but...
+        #get the last element instead of the first and solve the right tree first
+        idx_map={value:idx for idx,value in enumerate(inorder)}
+        def helper(left,right):
+            if left>right:
+                return None
+            root_val=postorder.pop()
+            root=TreeNode(root_val)
+            root.right=helper(idx_map[root_val]+1,right)
+            root.left=helper(left,idx_map[root_val]-1)
+            return root
+        return helper(0,len(postorder)-1)
