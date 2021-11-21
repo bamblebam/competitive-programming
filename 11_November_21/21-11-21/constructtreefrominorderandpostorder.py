e5695a3e7e2e6dcc19edd9564d4d37d654357053
bamblebam <burhanizrangwala@gmail.com>
2021-11-21 22:24:59 +0530

    11_November_21/21-11-21/constructtreefrominorderandpostorder:

--- 11_November_21/21-11-21/constructtreefrominorderandpostorder.py
+++ 11_November_21/21-11-21/constructtreefrominorderandpostorder.py
@@ -9,11 +9,14 @@ class Solution:
         #just like with preorder but...
         #get the last element instead of the first and solve the right tree first
         idx_map={value:idx for idx,value in enumerate(inorder)}
+        post=len(postorder)-1
         def helper(left,right):
+            nonlocal post
             if left>right:
                 return None
-            root_val=postorder.pop()
+            root_val=postorder[post]
             root=TreeNode(root_val)
+            post-=1
             root.right=helper(idx_map[root_val]+1,right)
             root.left=helper(left,idx_map[root_val]-1)
             return root
