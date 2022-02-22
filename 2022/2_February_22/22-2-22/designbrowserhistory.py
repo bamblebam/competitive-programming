5c834b0ac333e389555f29c553509a5d65ca095e
bamblebam <burhanizrangwala@gmail.com>
2022-02-22 14:33:24 +0530

    2022/2_February_22/22-2-22/designbrowserhistory: create

---
+++ 2022/2_February_22/22-2-22/designbrowserhistory.py
@@ -0,0 +1,24 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.stack=[homepage]
+        self.pointer=0
+
+    def visit(self, url: str) -> None:
+        print(self.stack)
+        self.stack=self.stack[:self.pointer+1]+[url]
+        self.pointer+=1
+
+    def back(self, steps: int) -> str:
+        self.pointer=max(self.pointer-steps,0)
+        return self.stack[self.pointer]
+
+    def forward(self, steps: int) -> str:
+        self.pointer=min(self.pointer+steps,len(self.stack)-1)
+        return self.stack[self.pointer]
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
