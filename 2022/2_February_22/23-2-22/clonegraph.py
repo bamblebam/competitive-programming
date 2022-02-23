255052fd0eeb8fad2b2679f1e530ffd822515c03
bamblebam <burhanizrangwala@gmail.com>
2022-02-23 11:18:13 +0530

    2022/2_February_22/23-2-22/clonegraph: create

---
+++ 2022/2_February_22/23-2-22/clonegraph.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        '''
+        use dfs
+        '''
+        def dfs(node):
+            mapping[node]=Node(node.val)
+            for neigh in node.neighbors:
+                if neigh not in mapping:
+                    dfs(neigh)
+                mapping[node].neighbors+=[mapping[neigh]]
+        
+        if not node:
+            return node
+        mapping=dict()
+        dfs(node)
+        return mapping[node]
